8e85ce868db88dd25b5561cd80a09fa9bda5e7c6
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-14 11:03:03 -0600

    Test: Add Iteration 4 tests to race_spec.

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Race do
       expect(@race).to be_a(Race)
       expect(@race.office).to eq("Texas Governor")
       expect(@race.candidates).to eq([])
+      expect(@race.open).to eq(true)
     end
   end
 
@@ -30,6 +31,75 @@ RSpec.describe Race do
       expect(@race.candidates).to eq([candidate_1])
       candidate_2 = @race.register_candidate!({ name: "Roberto R", party: :republican })
       expect(@race.candidates).to eq([candidate_1, candidate_2])
+    end
+  end
+
+  describe "#open?" do
+    it "can return the boolean status of the open attribute" do
+      expect(@race.open?).to eq(true)
+    end
+  end
+
+  describe "#close!" do
+    it "can change the boolean status of the open attribute to false" do
+      expect(@race.open).to eq(true)
+      @race.close!
+      expect(@race.open).to eq(false)
+    end
+  end
+
+  describe "#winner" do
+    it "returns false if race is still open" do
+      expect(@race.open?).to eq(true)
+      expect(@race.winner).to eq(false)
+    end
+
+    it "can return the candidate with the most votes" do
+      candidate_1 = @race.register_candidate!({ name: "Diana D", party: :democrat })
+      candidate_2 = @race.register_candidate!({ name: "Roberto R", party: :republican })
+
+      500.times { candidate_1.vote_for! }
+      5.times { candidate_2.vote_for! }
+
+      @race.close!
+      expect(@race.open?).to eq(false)
+      expect(@race.winner).to eq(candidate_1)
+    end
+
+    it "can return either candidate in the case of a tie" do
+      candidate_1 = @race.register_candidate!({ name: "Diana D", party: :democrat })
+      candidate_2 = @race.register_candidate!({ name: "Roberto R", party: :republican })
+
+      50.times { candidate_1.vote_for! }
+      50.times { candidate_2.vote_for! }
+
+      @race.close!
+      expect(@race.open?).to eq(false)
+      expect(@race.winner).to eq(candidate_1 || candidate_2)
+    end
+  end
+
+  describe "#tie?" do
+    it "returns false if candidates have a different number votes" do
+      candidate_1 = @race.register_candidate!({ name: "Diana D", party: :democrat })
+      candidate_2 = @race.register_candidate!({ name: "Roberto R", party: :republican })
+
+      500.times { candidate_1.vote_for! }
+      5.times { candidate_2.vote_for! }
+
+      @race.close!
+      expect(@race.tie?).to eq(false)
+    end
+
+    it "returns true if candidates have the same number of votes" do
+      candidate_1 = @race.register_candidate!({ name: "Diana D", party: :democrat })
+      candidate_2 = @race.register_candidate!({ name: "Roberto R", party: :republican })
+
+      50.times { candidate_1.vote_for! }
+      50.times { candidate_2.vote_for! }
+
+      @race.close!
+      expect(@race.tie?).to eq(true)
     end
   end
 end
